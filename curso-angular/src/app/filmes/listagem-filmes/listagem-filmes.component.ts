d3984df765243937b718335d70711d76d3aaa49c
Robert Santos <robertsantosti@outlook.com>
2020-02-25 12:37:53 -0300

    Criando formulario de filtro

--- curso-angular/src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ curso-angular/src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder } from "@angular/forms";
 import { FilmsService } from "src/app/core/films.service";
 import { Film } from "src/app/shared/models/film";
 
@@ -10,11 +11,28 @@ import { Film } from "src/app/shared/models/film";
 export class ListagemFilmesComponent implements OnInit {
   films: Film[] = [];
   page = 0;
+  filterList: FormGroup;
+  genders: Array<string>;
   readonly quant = 4;
 
-  constructor(private filmsService: FilmsService) {}
+  constructor(private filmsService: FilmsService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.filterList = this.fb.group({
+      text: [""],
+      gender: [""]
+    });
+
+    this.genders = [
+      "Ação",
+      "Romance",
+      "Aventura",
+      "Terror",
+      "Ficção Cientifica",
+      "Comedia",
+      "Drama"
+    ];
+
     this.showFilms();
   }
 
